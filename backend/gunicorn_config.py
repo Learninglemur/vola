806362f2bfcafa7fe523f75e8693e283db6412ee
Learninglemur <arajaobelina241@gmail.com>
2024-12-01 16:05:21 -0600

    Create gunicorn_config.py

---
+++ backend/gunicorn_config.py
@@ -0,0 +1,8 @@
+# gunicorn_config.py
+import os
+
+workers = int(os.getenv('WORKERS', 1))
+threads = int(os.getenv('THREADS', 2))
+timeout = int(os.getenv('TIMEOUT', 0))
+bind = f"0.0.0.0:{os.getenv('PORT', 8080)}"
+worker_class = 'uvicorn.workers.UvicornWorker'
